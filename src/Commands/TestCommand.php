750db25b0e22ba20f26c69d4ccdb8ffd3e99098a
ankogit <ya.tolik-a@yandex.ru>
2022-06-11 14:55:19 +0300

    init

---
+++ src/Commands/TestCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MyVendor\MyPackage\Commands;
+
+
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'test desc';
+
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+
+     */
+    public function handle():void
+    {
+        $this->line("test line!");
+    }
+
+
+}
